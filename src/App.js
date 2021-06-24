e4a29e2653891c319d369f943cbf2360e4f97a2a
Leandro Reis <lleandrofr@gmail.com>
2021-06-24 15:59:04 -0300

    fix lint errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Provider from './context/Provider'
+import Provider from './context/Provider';
 import './App.css';
 import Table from './components/Table';
 
-
 function App() {
   return (
-    <Provider >
+    <Provider>
       <span>A long time agora in a galaxy far, far away...</span>
       <Table />
     </Provider>
