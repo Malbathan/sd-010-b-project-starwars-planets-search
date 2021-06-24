09ad7b39e0cf10bdfc1acf810aed9e41ead426df
EderPaiva <edergomesdepaiva12@gmail.com>
2021-06-24 14:51:17 -0300

    fetch feito, construido o objeto e armazenado no estado proximo passo? enviar indentificar que precisara do context e o que precisara

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect , userState} from 'react';
 import Table from './Table';
+import getplanets from '../services/data';
 
  function Main() {
+     const [data, setData] = useState([])
+     
+    useEffect(() => {
+        const dataSet = async () => {
+            const res = await getplanets();
+            setData(res)
+        }
+     }, [])
+  
     return (
         <Table />
     )
-}
+
+ }
+
 export default Main;
---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// estrutura da tabela
+// tr é linha table row
+// td é coluna table division
+
+ function Table() {
+    return (
+        <div></div>
+    )
+}
+export default Table;
