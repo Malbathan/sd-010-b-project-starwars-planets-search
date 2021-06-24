75a095b9eba25359a88084c93ca7d97d52c186a7
Leandro Reis <lleandrofr@gmail.com>
2021-06-24 18:05:09 -0300

    creates a some dropdown list for the numeric filters

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,27 @@
 import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
+const numberValuesOptions = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
+
+const comparisonFilters = [
+  'maior que',
+  'menor que',
+  'igual a',
+];
+
 function Filter() {
   const { handleChangeText, textFilter } = useContext(PlanetsContext);
   return (
     <form>
+
       <label htmlFor="name-filter">
-        Name:
+        Nome:
         <input
           type="text"
           data-testid="name-filter"
@@ -15,6 +30,30 @@ function Filter() {
           onChange={ handleChangeText }
         />
       </label>
+
+      <label htmlFor="column-filter">
+        Filtrar por:
+        <select name="column" id="column-filter" data-testid="column-filter">
+          {numberValuesOptions.map((el, idx) => (
+            <option key={ idx }>{el}</option>
+          ))}
+        </select>
+      </label>
+
+      <select name="comparation" id="comparation" data-testid="comparison-filter">
+        {comparisonFilters.map((el, idx) => (
+          <option key={ idx }>{el}</option>
+        ))}
+      </select>
+
+      <input
+        type="number"
+        data-testid="value-filter"
+        id="value-filter"
+      />
+
+      <button type="button" data-testid="button-filter">Filtrar</button>
+
     </form>
   );
 }
