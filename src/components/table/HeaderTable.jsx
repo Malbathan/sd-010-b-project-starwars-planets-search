3eb2e956bb745ad89ff9d587c490cfd7a0b79511
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-24 18:22:55 -0300

    Cria o header da tabela

---
+++ src/components/table/HeaderTable.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function HeaderTable() {
+  return (
+    <tr>
+      <th>Nome</th>
+      <th>Período de Rotação</th>
+      <th>Período orbital</th>
+      <th>Diâmetro</th>
+      <th>Clima</th>
+      <th>Gravidade</th>
+      <th>Terreno</th>
+      <th>Água na superfície</th>
+      <th>População</th>
+      <th>Filmes</th>
+      <th>Criação</th>
+      <th>Edição</th>
+      <th>Url</th>
+    </tr>
+  );
+}
+
+export default HeaderTable;
