b810636dec5569f716f64314ef785fd1dfc3cf7d
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-24 16:40:44 -0300

    Criando select column

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,15 +4,26 @@ import { Context } from '../contexts';
 function Input() {
   const { input, setInput } = useContext(Context);
   return (
-    <label htmlFor="name">
-      <input
-        data-testid="name-filter"
-        id="name"
-        type="text"
-        value={ input }
-        onChange={ ({ target }) => setInput(target.value) }
-      />
-    </label>
+    <>
+      <label htmlFor="name">
+        <input
+          data-testid="name-filter"
+          id="name"
+          type="text"
+          value={ input }
+          onChange={ ({ target }) => setInput(target.value) }
+        />
+      </label>
+      <label htmlFor="column">
+        <select data-testid="column-filter" id="column">
+          <option value="population">Population</option>
+          <option value="orbital_period">Orbital Period</option>
+          <option value="diameter">Diameter</option>
+          <option value="rotation_period">Rotation Period</option>
+          <option value="surface_water">Surface Water</option>
+        </select>
+      </label>
+    </>
   );
 }
 
