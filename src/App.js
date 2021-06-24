319bd414d71adfbd7cc7b9813c1475c891ca74f0
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-24 15:07:35 -0300

    [RAPHAEL GUMIERI] Project Star Wars Planets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
+// comentario para p primeiro commit
 
 function App() {
-  return (
-    <span>Hello, App!</span>
-  );
+  return <span>Hello, App!</span>;
 }
 
 export default App;
