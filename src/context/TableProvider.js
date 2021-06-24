63545d3078727ed8e501e16ebdafb331128337c2
bidaffonso <bidaffondo@gmail.com>
2021-06-24 15:40:01 -0400

    Stop coding because of internet issues

--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Proptypes from 'prop-types';
 import TableContext from './TableContext';
+import getPlanetsFromAPI from '../services';
 
 function TableProvider({ children }) {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const results = await getPlanetsFromAPI();
+      console.log(results);
+      // setPlanets({
+      //   results,
+      // });
+    };
+    getPlanets();
+  });
+
   return (
     <TableContext.Provider>
       {children}
