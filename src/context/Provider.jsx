ac25a09686d4f24bc2a23a9854952385032f8972
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-06-24 19:10:25 -0300

    agora com context

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -2,28 +2,32 @@ import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import PlanetContext from './PlanetContext';
 
-function Provider({ children }) {
-  const [data, setData] = useState(undefined);
+function PlanetProvider({ children }) {
+  const [data, setData] = useState([]);
 
   async function getUser() {
     const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
     const response = await fetch(url);
     const { results } = await response.json();
     setData(results);
+    console.log(results);
   }
 
   useEffect(() => {
     getUser();
   }, []);
+
+  const globalContext = {
+    data,
+  };
   return (
-    <PlanetContext.Provider value={ data }>
+    <PlanetContext.Provider value={ globalContext }>
       {children}
     </PlanetContext.Provider>
   );
 }
 
-Provider.propTypes = {
-  children: PropTypes.arrayOf.isRequired,
-};
-
-export default Provider;
+PlanetProvider.propTypes = {
+  children: PropTypes.html,
+}.isRequired;
+export default PlanetProvider;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Provider from './context/Provider';
 import App from './App';
+import Provider from './context/Provider';
 
 ReactDOM.render(
   <Provider>
