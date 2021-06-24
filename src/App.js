9b37f73711c05c34fea588375f77c89eff81ef96
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-06-24 20:28:02 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
+import FiltraNome from './components/FiltraNome';
 import SearchProvider from './context/SearchProvider';
 
 function App() {
   return (
     <SearchProvider>
+      <FiltraNome />
       <Table />
     </SearchProvider>
   );
