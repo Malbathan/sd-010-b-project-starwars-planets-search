67a14682ffc5f67aadacd3a40ad78fe6a146075b
isTorres <ismaeltorres612@gmail.com>
2021-06-24 17:58:31 -0300

    create func to render header of table

--- src/App.js
+++ src/App.js
@@ -4,6 +4,31 @@ import StarWarsContext from './context/StarWarsContext';
 import './App.css';
 
 function App() {
+  const { planets, setPlanets } = useContext(StarWarsContext);
+  const tableHeader = planets[0];
+  const loading = <h2>Loading</h2>;
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const { results } = await fetch(endpoint).then((data) => data.json());
+      setPlanets(results);
+    };
+    getPlanets();
+  }, [setPlanets]);
+
+  const renderTH = () => {
+    if (tableHeader) {
+      return Object.keys(tableHeader).filter((el) => el !== 'residents')
+        .map((entrie, i) => <th key={ i }>{entrie}</th>);
+    }
+    return loading;
+  };
+
+  // const renderTBody = () => {
+  //   if (tableHeader)
+  // }
+
   return (
     <span>Hello, App!</span>
   );
