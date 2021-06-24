8dffeaea45e17943a8e20d113706edc19bf68f2e
Mariana Mohr <marianamohr@gmail.com>
2021-06-24 18:31:50 -0300

    Ajuste table

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -4,40 +4,46 @@ import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
   const { data } = useContext(StarWarsContext);
-  console.log(data);
+
   return (
     <table>
-      <tr>
-        <th>Name</th>
-        <th>climate</th>
-        <th>created</th>
-        <th>diameter</th>
-        <th>Films</th>
-        <th>edited</th>
-        <th>Gravity</th>
-        <th>Orbital_period</th>
-        <th>Population</th>
-        <th>rotation_period</th>
-        <th>surface_water</th>
-        <th>terrain</th>
-        <th>url</th>
-      </tr>
-      {data.map((planet) => (
-        <tr key={ planet.name }>
-          <td>{planet.name}</td>
-          <td>{planet.climate}</td>
-          <td>{planet.created}</td>
-          <td>{planet.diameter}</td>
-          <td>{planet.films}</td>
-          <td>{planet.edited}</td>
-          <td>{planet.gravity}</td>
-          <td>{planet.orbital_period}</td>
-          <td>{planet.population}</td>
-          <td>{planet.rotation_period}</td>
-          <td>{planet.surface_water}</td>
-          <td>{planet.terrain}</td>
-          <td>{planet.url}</td>
-        </tr>))}
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>climate</th>
+          <th>created</th>
+          <th>diameter</th>
+          <th>Films</th>
+          <th>edited</th>
+          <th>Gravity</th>
+          <th>Orbital_period</th>
+          <th>Population</th>
+          <th>rotation_period</th>
+          <th>surface_water</th>
+          <th>terrain</th>
+          <th>url</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((planet) => (
+
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.created}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.films}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.population}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
