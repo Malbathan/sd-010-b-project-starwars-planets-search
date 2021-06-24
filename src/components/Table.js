6bb3948a0cbc79092ea19dae75e50b852f201d8a
Jessie <miss_sc_jessi@hotmail.com>
2021-06-24 18:54:40 -0300

    requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import starwarsContext from '../context/starwarsContext';
 
 function Table() {
+  const [filter, setFilter] = useState('');
   const { data } = useContext(starwarsContext);
+  const handleChange = ({ target: { value } }) => setFilter(value);
   if (!data) return <p>Carregando...</p>;
   return (
     <>
+      <input data-testid="name-filter" onChange={ handleChange } />
       <tr>
         <th>name</th>
         <th>rotation period</th>
@@ -21,7 +24,7 @@ function Table() {
         <th>edited</th>
         <th>url</th>
       </tr>
-      { data.map((planets, index) => (
+      { data.filter((planet) => planet.name.includes(filter)).map((planets, index) => (
         <tr key={ index }>
           {Object.values(planets)
             .map((values, indexs) => <td key={ indexs }>{values}</td>)}
