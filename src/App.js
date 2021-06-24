cce54b4b85aedfed4b4cec1c26ce728f42d494a3
DennisMarcelo <dennismarcelo15@gmail.com>
2021-06-24 16:35:56 -0300

    fetch sendo buscado como sucesso

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+
+import Table from './components/Table'
+import ContextStarWars from './context/ContextStarWars';
 import './App.css';
 
 function App() {
+  const { data, setData } = useContext(ContextStarWars);
+
+  useEffect(() => {
+    async function featchPlanets() {
+      const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const objResponse = await fetch(url).then((results) => results.json());
+      const arrayPlanets = objResponse.results.map((obj) => {
+        delete obj.residents;
+        return obj;
+      });
+      setData(arrayPlanets);
+    }
+
+    featchPlanets();
+  }, [setData]);
+
   return (
-    <span>Hello, App!</span>
+    <Table />
   );
 }
 
