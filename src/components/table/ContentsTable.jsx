ddbafd49ddfa67ae7e86146fbaf776d484dbcc26
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-24 21:57:47 -0300

    Adiciona o filtro do componenente ContentsTable

--- src/components/table/ContentsTable.jsx
+++ src/components/table/ContentsTable.jsx
@@ -2,26 +2,39 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../../context/StarWarsContext';
 
 function ContentsTable() {
-  const { data } = useContext(StarWarsContext);
+  const { data, filters } = useContext(StarWarsContext);
 
-  const dadosPlanets = data.map((valueDados, index) => (
-    <tr key={ index }>
-      <td>{valueDados.name}</td>
-      <td>{valueDados.rotation_period}</td>
-      <td>{valueDados.orbital_period}</td>
-      <td>{valueDados.diameter}</td>
-      <td>{valueDados.climate}</td>
-      <td>{valueDados.gravity}</td>
-      <td>{valueDados.terrain}</td>
-      <td>{valueDados.surface_water}</td>
-      <td>{valueDados.population}</td>
-      <td>{valueDados.films}</td>
-      <td>{valueDados.created}</td>
-      <td>{valueDados.edited}</td>
-      <td>{valueDados.url}</td>
-    </tr>
-  ));
-  return dadosPlanets;
+  const { filterByName: { name } } = filters;
+
+  console.log(name);
+
+  // const filterNamePlanets = data.filter(({ name }) => name.toLocaleLowerCase()
+  //   .includes(name.toLocaleLowerCase()));
+
+  const filterNamePlanets = (name !== '')
+    ? data.filter((value) => value.name.toLocaleLowerCase()
+      .includes(name))
+    : data;
+
+  return (
+    filterNamePlanets.map((valueDados, index) => (
+      <tr key={ index }>
+        <td>{valueDados.name}</td>
+        <td>{valueDados.rotation_period}</td>
+        <td>{valueDados.orbital_period}</td>
+        <td>{valueDados.diameter}</td>
+        <td>{valueDados.climate}</td>
+        <td>{valueDados.gravity}</td>
+        <td>{valueDados.terrain}</td>
+        <td>{valueDados.surface_water}</td>
+        <td>{valueDados.population}</td>
+        <td>{valueDados.films}</td>
+        <td>{valueDados.created}</td>
+        <td>{valueDados.edited}</td>
+        <td>{valueDados.url}</td>
+      </tr>
+    ))
+  );
 }
 
 export default ContentsTable;
