a470d4580b2ee521f79ad76c738d7db72ca885d1
walterlbf <walter.lbf.f@gmail.com>
2021-06-25 17:12:26 -0400

    criaada funcao que salva o input no state

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -4,11 +4,19 @@ import Context from '../context/Context';
 function Filters(params) {
   const { filters, setFilters } = useContext(Context);
 
+  const handleChange = ({ target: { value } }) => {
+    setFilters({
+      ...filters,
+      filterByName: { name: value },
+    });
+  };
+
   return (
     <header>
       <input
         type="text"
         data-testid="name-filter"
+        onChange={ handleChange }
         placeholder="digite o nome do filme"
       />
     </header>
