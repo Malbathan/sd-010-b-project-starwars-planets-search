379ebb8acb77de24d3f1f19c16dad31ca3ef51b3
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-06-25 00:23:21 -0300

    lint refeito

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from '../Context/StarWarsContext';
 import useFetchApiPlanets from '../Hooks/apiPlanets';
 import useFiltering from '../Hooks/filtering';
