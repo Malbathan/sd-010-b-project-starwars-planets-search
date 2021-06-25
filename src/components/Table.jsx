42bc6a116067ad96d20afaaec2d0487e91412c1e
walterlbf <walter.lbf.f@gmail.com>
2021-06-25 16:31:39 -0400

    identacao

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,29 +7,32 @@ function Table() {
   } = useContext(Context);
 
   return (
-    !planetsList.length ? <h2>Loading...</h2>
-      : <table>
-        <thead>
-          <tr>
-            {
-              Object.keys(planetsList[0]).map(
-                (eachKey) => <th key={ eachKey }>{eachKey}</th>,
-              )
-            }
-          </tr>
-        </thead>
-        <tbody>
-          {planetsList.map((eachElement) => (
-            <tr key={ eachElement.name }>
-              {Object.values(eachElement).map((eachValue) => (
-                <td key={ eachValue }>
-                  {eachValue}
-                </td>
-              ))}
+    !planetsList.length
+      ? <h2>Loading...</h2>
+      : (
+        <table>
+          <thead>
+            <tr>
+              {
+                Object.keys(planetsList[0]).map(
+                  (eachKey) => <th key={ eachKey }>{eachKey}</th>,
+                )
+              }
             </tr>
-          ))}
-        </tbody>
+          </thead>
+          <tbody>
+            {planetsList.map((eachElement) => (
+              <tr key={ eachElement.name }>
+                {Object.values(eachElement).map((eachValue) => (
+                  <td key={ eachValue }>
+                    {eachValue}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
         </table>
+      )
   );
 }
 
