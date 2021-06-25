5f6d784da070cd8310c15ea58f8491e33f6701b2
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-24 21:56:23 -0300

    Rederiza o input de filtro por nome no componente app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import InputFilter from './components/Inputs/InputFilter';
 import Table from './components/table/Table';
 import StarWarsProvider from './context/StarWarsProvider';
 
 function App() {
   return (
     <StarWarsProvider>
+      <InputFilter />
       <Table />
     </StarWarsProvider>
   );
