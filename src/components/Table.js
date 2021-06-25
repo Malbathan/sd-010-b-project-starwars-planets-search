6521e838f2065bbd8258da5ce0ac32755fa9cd04
EderPaiva <edergomesdepaiva12@gmail.com>
2021-06-25 08:15:09 -0300

    requisito 3 filtros verbosos rsrsrs

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import TableContext from '../context/tablecontext';
+// option colums ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water']
+// option ['maior que', 'igual a', 'menor que']
 
 function Table() {
   const [filter, setFilter] = useState(['']);
@@ -8,10 +10,26 @@ function Table() {
 
   useEffect(() => (
     filter === '' ? setFilteredPlanets(data)
-      : setFilteredPlanets(data.filter((planet) => planet.name.match(filter)))), [data, filter]);
-
+      : (
+        setFilteredPlanets(data.filter((planet) => (
+          (planet.name.match(filter))))))), [data, filter]);
+  // rei dos parenteses
+  console.log(data);
   return (
     <>
+      <select name="column-filter" data-testid="column-filter">
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select name="comparison-filter" data-testid="comparison-filter">
+        <option value="maior que">maior que</option>
+        <option value="igual a">igual a</option>
+        <option value="menor que">menor que</option>
+      </select>
+      <input type="number" data-testid="value-filter" />
       <input
         type="text"
         placeholder="FilterByName"
