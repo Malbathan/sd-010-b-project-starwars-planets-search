e045b8f604b8302fed7055d4de021127cdddb476
Italo Matos <italomatos764@gmail.com>
2021-06-25 12:14:52 -0400

    Tentando fazer req 2

--- src/components/StarWarsProvider.js
+++ src/components/StarWarsProvider.js
@@ -3,27 +3,52 @@ import PropTypes from 'prop-types';
 import StarWarsContext from './StarWarsContext';
 
 function Provider({ children }) {
-  const [responseAPI, setResponseAPI] = useState([]);
+  const [planetNames, setplanetNames] = useState([]);
+  const [nameSearch, setNameSearch] = useState('');
+  const [filter, setFilter] = useState({});
+  // const [planetsFiltered, setPlanetsFiltered] = useState([]);
 
-  const fetchAPI = async () => {
+  const fetchAPI = () => {
     // const apiStarWars = 'https://swapi-trybe.herokuapp.com/api/planets/?format=json';
     fetch('https://swapi-trybe.herokuapp.com/api/planets/?format=json')
       .then((response) => response.json())
-      .then((resp) => setResponseAPI(resp.results));
+      .then((resp) => setplanetNames(resp.results));
   };
 
   useEffect(() => {
     fetchAPI();
   }, []);
 
-  const stateGlobal = {
-    fetchAPI,
-    responseAPI,
+  const handleInputSearch = (event) => {
+    setNameSearch(event.target.value);
+    // const planetsSearch = planetNames.filter((planet) => {
+    //   const name = planet.name.toLowerCase();
+    //   const input = event.target.value.toLowerCase();
+    //   return name.includes(input);
+    // });
+
+    // const filterByName = {
+    //   name: event.target.value,
+    // };
+
+    // setFilter({ ...filter, filterByName });
+    // setplanetNames(planetsSearch);
+    // setPlanetsFiltered(planetsSearch);
+  };
+
+  const values = {
+    planetNames,
+    handleInputSearch,
+    filters: {
+      filterByName: {
+        name: nameSearch,
+      },
+    },
   };
 
   return (
     <div>
-      <StarWarsContext.Provider value={ stateGlobal }>
+      <StarWarsContext.Provider value={ values }>
         {children}
       </StarWarsContext.Provider>
     </div>
