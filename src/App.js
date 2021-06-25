85961e369c5f3187e55eebace52710cf6b08b28f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-25 00:14:53 -0300

    adicionando o componente filters no App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Table from './components/Table';
 import { Provider } from './components/PlanetsProvider';
+import Filters from './components/Filters';
 
 function App() {
   return (
     <Provider>
+      <Filters />
       <Table />
     </Provider>
   );
