4450bd184c3ab9eb9bc3f64af315de7511c841fd
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-26 07:25:02 -0300

    chamando a API no user efects

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PlanetsContext from './PlanetsContext';
 
 function PlanetsProvider({ children }) {
-  const [data, setData] = useState();
-  fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-    .then((response) => response.json())
-    .then(() => setData(data));
+  const [data, setData] = useState([]);
+
+  const fetchApi = async () => {
+    const request = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const result = await request.json();
+    setData(result.results);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
-    <PlanetsContext.Provider value={ {} }>
+    <PlanetsContext.Provider value={ { data, fetchApi } }>
       {children}
     </PlanetsContext.Provider>
   );
