adbe7a71a8b8ed02ae807764fc18d6390d253143
André Arnoni <andre_arnoni@yahoo.com.br>
2021-06-25 22:43:14 -0300

    executados requisitos 3 e 4

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,20 +2,54 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { data, head, filters, handleChange } = useContext(PlanetsContext);
-  const { filterByName: { name } } = filters;
+  const {
+    filter,
+    columns,
+    search,
+    head,
+    filters,
+    filterByParameters,
+    handleChange } = useContext(PlanetsContext);
+  const { name } = filters;
 
   return (
     <div>
-      <label htmlFor="name">
-        Search by name:
-        <input
-          type="search"
-          data-testid="name-filter"
-          id="name"
-          onChange={ handleChange }
-        />
-      </label>
+      <select name="column" data-testid="column-filter" onChange={ handleChange }>
+        {columns.map((item, index) => (
+          <option key={ index } value={ Object.values(item) }>
+            { Object.values(item) }
+          </option>
+        ))}
+      </select>
+      <select data-testid="comparison-filter" name="comparison" onChange={ handleChange }>
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="text"
+        data-testid="value-filter"
+        name="value"
+        onChange={ handleChange }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ () => { filterByParameters(); filter(); } }
+      >
+        Filtro
+      </button>
+      <div>
+        <label htmlFor="name">
+          Search by name:
+          <input
+            type="search"
+            name="name"
+            data-testid="name-filter"
+            onChange={ handleChange }
+          />
+        </label>
+      </div>
       <table border="1">
         <thead>
           <tr>
@@ -23,11 +57,12 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {data.filter((info) => {
+          {search.filter((info) => {
             if (name === '') {
               return info;
-            } return (info.name.toLowerCase()
-              .includes(name.toLocaleLowerCase()));
+            }
+            return info.name.toLowerCase()
+              .includes(name.toLocaleLowerCase());
           }).map((info, index) => (
             <tr key={ index }>
               {head.map((item, index1) => (
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -5,17 +5,27 @@ import fetchPlanets from '../services/fetchPlanets';
 
 function Provider({ children }) {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState([]);
   const [head, setHead] = useState([]);
   const [filters, setFilters] = useState({
-    filterByName: {
-      name: '',
-    },
+    name: '',
+    column: '',
+    comparison: '',
+    value: 0,
   });
+  const [columns, setColumns] = useState([
+    { population: 'population' },
+    { orbital_period: 'orbital_period' },
+    { diameter: 'diameter' },
+    { rotation_period: 'rotation_period' },
+    { surface_water: 'surface_water' },
+  ]);
 
   useEffect(() => {
     const getPlanets = async () => {
       const request = await fetchPlanets();
       setData(request);
+      setSearch(request);
       setHead((Object.keys(request[0]))
         .filter((item) => item !== 'residents')
         .map((item) => item));
@@ -24,20 +34,47 @@ function Provider({ children }) {
   }, []);
 
   const handleChange = ({ target }) => {
-    const { value } = target;
+    const { name, value } = target;
     setFilters({
       ...filters,
-      filterByName: {
-        name: value,
-      },
+      [name]: value,
     });
   };
 
+  const filterByParameters = () => {
+    const { column, comparison, value } = filters;
+    let turn = data;
+    switch (comparison) {
+    case 'maior que':
+      turn = data.filter((item) => item[column] > Number(value));
+      break;
+    case 'menor que':
+      turn = data.filter((item) => item[column] < Number(value));
+      break;
+    case 'igual a':
+      turn = data.filter((item) => item[column] === value);
+      break;
+    default:
+      turn = data;
+      break;
+    }
+    setSearch(turn);
+  };
+
+  const filter = (id) => {
+    const { column } = filters;
+    setColumns((item) => item.filter((q) => q[column] === id));
+  };
+
   const context = {
+    filter,
+    columns,
+    search,
     data,
     head,
     filters,
     handleChange,
+    filterByParameters,
   };
 
   return (
