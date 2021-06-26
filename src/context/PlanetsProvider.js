807ada0a0bb27d17488367b38a733cd726898669
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-26 06:52:00 -0300

    fazendo a chamada na API e inserindo no data

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlanetsContext from './PlanetsContext';
 
 function PlanetsProvider({ children }) {
+  const [data, setData] = useState();
+  fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+    .then((response) => response.json())
+    .then(() => setData(data));
   return (
     <PlanetsContext.Provider value={ {} }>
       {children}
