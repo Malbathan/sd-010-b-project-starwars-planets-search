6101895660e2379a3fdb49698b7107b6aaa35740
isTorres <ismaeltorres612@gmail.com>
2021-06-26 00:14:56 -0300

    componentizei o input de filtro por nome

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import newPlanets from './Components/newPlanets';
+import renderTB from './Components/tableBody';
+import SWStateContext from './context/SWContext';
+import Filters from './Components/FilterByName';
 import './App.css';
 
 function App() {
   const [planets, setPlanets] = useState([]);
-  // const [filter, setFilter] = useState([]);
-
   const frstElemnt = planets[0];
+  // pega o 1 obj vindo da api e filtra suas keys p/ usar no cabeçalho da tabela
   const tableHead = frstElemnt ? Object.keys(frstElemnt)
     .filter((el) => el !== 'residents') : null;
 
+  // constroi o cabeçalho da tabela
+  const renderTH = (arrayOfData) => {
+    if (frstElemnt) {
+      return <tr>{arrayOfData.map((data, i) => <th key={ i }>{data}</th>)}</tr>;
+    }
+  };
+
+  // Faz requisição na api
   useEffect(() => {
     const getPlanets = async () => {
       const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
@@ -18,68 +29,24 @@ function App() {
     getPlanets();
   }, [setPlanets]);
 
-  const newPlanets = () => {
-    // remove a specific key of object: https://stackoverflow.com/questions/38750705/filter-object-properties-by-key-in-es6
-    if (frstElemnt) {
-      const response = planets.map(
-        (planet) => {
-          const filtered = Object.keys(planet)
-            .filter((key) => tableHead.includes(key))
-            .reduce((obj, key) => {
-              obj[key] = planet[key];
-              return obj;
-            }, {});
-          return filtered;
-        },
-      );
-      return response;
-    }
-  };
-
-  const renderTH = (arrayOfData) => {
-    if (frstElemnt) {
-      return <tr>{arrayOfData.map((data, i) => <th key={ i }>{data}</th>)}</tr>;
-    }
-  };
-
-  const renderTB = () => {
-    if (frstElemnt) {
-      const official = newPlanets().map((el) => Object.values(el));
-      const filtrada = official
-        .map((array, i) => (
-          <tr key={ i }>
-            { array
-              .map((el, index) => <td key={ index }>{el}</td>) }
-          </tr>));
-      console.log(official);
-      return filtrada;
-    }
-  };
-
-  // const searchFilter = () => {
-  //   console.log(filter);
-  //   console.log(setFilter);
-  // };
+  // pega o newPlanets(array da api filtrado) e mapeia p/ preencher a tabela
+  const arrayOfPlanetsData = frstElemnt
+    ? newPlanets(frstElemnt, planets, tableHead) : null;
 
   return (
-    <main className="App">
-      <form>
-        <input
-          placeholder="Buscar"
-          maxLength="30"
-          data-testid="name-filter"
-          // onChange={ searchFilter }
-        />
-      </form>
-      <table className="table">
-        <thead>
-          { renderTH(tableHead) }
-        </thead>
-        <tbody>
-          { renderTB() }
-        </tbody>
-      </table>
-    </main>
+    <SWStateContext>
+      <main className="App">
+        <Filters />
+        <table className="table">
+          <thead>
+            { renderTH(tableHead) }
+          </thead>
+          <tbody>
+            { renderTB(frstElemnt, arrayOfPlanetsData) }
+          </tbody>
+        </table>
+      </main>
+    </SWStateContext>
   );
 }
 
