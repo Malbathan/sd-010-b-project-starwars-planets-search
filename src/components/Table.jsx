ba3096fc546d54683b451caea734da342492572c
Carlos Vieira <appencigr@gmail.com>
2021-06-26 14:13:35 -0300

    Requisito 3 Completo

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,69 +2,61 @@ import React, { useContext } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 function Table() {
-  const { data, planets, setPlanets } = useContext(PlanetContext);
-
-  const clearFilter = () => {
-    setPlanets(data);
-  };
-
-  // {
-  //   filters: {
-  //     filterByName: {
-  //       name: 'Tatoo',
-  //     };
-  //   };
-  // };
-
-  const handleFilterNome = ({ target: { value } }) => {
-    const filteredName = data
-      .filter((element) => (element.name.includes(value)));
-    setPlanets(filteredName);
-  };
-
-  // const handleFilterNumber = ({ target: { value } }) => {
-  //   const filteredRotationPeriod = planets
-  //     .filter((element) => (element.rotation_period.includes(value)));
-  //   setPlanets(filteredRotationPeriod);
-  //   const filteredOrbitalPeriod = planets
-  //     .filter((element) => (element.orbital_period.includes(value)));
-  //   setPlanets(filteredOrbitalPeriod);
-  // };
+  const { data, search, handleChange,
+    handleChangeFilter, onClick } = useContext(PlanetContext);
 
   return (
     <main>
       <h1>Minha Tabela</h1>
+      <label htmlFor="name-filter">
+        Busca por texto
+        <input
+          type="text"
+          name="filterByName"
+          placeholder="filtrar por texto"
+          data-testid="name-filter"
+          onChange={ handleChange }
+        />
+      </label>
+      <select
+        name="column"
+        id="column-filter"
+        defaultValue="DEFAULT"
+        data-testid="column-filter"
+        onChange={ handleChangeFilter }
+      >
+        <option value="DEFAULT" disabled>Selecione tipo do filtro</option>
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        name="comparison"
+        defaultValue="DEFAULT"
+        data-testid="comparison-filter"
+        onChange={ handleChangeFilter }
+      >
+        <option value="DEFAULT" disabled>Selecione a comparação do filtro</option>
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
       <input
-        type="text"
-        name="filterByName"
-        placeholder="filtrar"
-        data-testid="name-filter"
-        onChange={ (event) => (handleFilterNome(event)) }
-        // onChange={ (event) => console.log(typeof event.target.value) }
-      />
-      {/* <input
         type="number"
-        name="filterByNumber"
-        placeholder="filtrar"
-        data-testid="number-filter"
-        // onChange={ (event) => (typeOf(event.target.value) === 'number'
-        //   ? handleFilterNumber(event)
-        //   : handleFilterNome(event)) }
-        onChange={ (event) => console.log(typeof event.target.value) }
-        onKeyDown={ () => clearFilter() }
-      /> */}
-      {/* <select name="" id="">
-        <option selected>Selecione</option>
-        {planets.map((planet, index) => (
-          <option
-            key={ index }
-            onKeyDown={ () => handleFilterNome() }
-          >
-            { planet.name }
-          </option>
-        ))}
-      </select> */}
-      <input type="button" value="X" onClick={ clearFilter } />
+        name="value"
+        placeholder="filtrar por número"
+        data-testid="value-filter"
+        onChange={ handleChangeFilter }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ onClick }
+      >
+        Filtrar
+      </button>
       <table>
         <thead>
           <tr>
@@ -84,8 +76,8 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {planets.map((planet, index) => (
-            <tr key={ index }>
+          {data.filter((item) => item.name.includes(search)).map((planet) => (
+            <tr key={ planet.name }>
               <td>{ planet.name }</td>
               <td>{ planet.rotation_period }</td>
               <td>{ planet.orbital_period }</td>
--- src/context/PlanetProvider.jsx
+++ src/context/PlanetProvider.jsx
@@ -5,29 +5,56 @@ import fetchPlanetsAPI from '../services/planetsAPI';
 
 function PlanetProvider({ children }) {
   const [data, setData] = useState([]);
-  const [planets, setPlanets] = useState([]);
-  // const [filters, setFilters] = useState({});
-
-  // {
-  //   filters: {
-  //     filterByName: {
-  //       name: 'Tatoo',
-  //     };
-  //   };
-  // };
+  const [search, setSearch] = useState('');
+  const [filterByNumericValues, setfilterByNumericValues] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: '100000',
+  });
 
   const dataAPI = async () => {
     const response = await fetchPlanetsAPI();
     setData(response.results);
-    setPlanets(response.results);
   };
 
   useEffect(() => {
     dataAPI();
   }, []);
 
+  const handleChange = ({ target: { value } }) => {
+    setSearch(value);
+  };
+
+  const handleChangeFilter = ({ target: { name, value } }) => {
+    setfilterByNumericValues({ ...filterByNumericValues, [name]: value });
+  };
+
+  function onClick() {
+    const { column, comparison, value } = filterByNumericValues;
+    const filtered = data.filter((item) => {
+      switch (comparison) {
+      case 'maior que':
+        return Number(item[column]) > Number(value);
+      case 'menor que':
+        return Number(item[column]) < Number(value);
+      default:
+        return Number(item[column]) === Number(value);
+      }
+    });
+    return setData(filtered);
+  }
+
   return (
-    <PlanetContext.Provider value={ { data, setData, planets, setPlanets } }>
+    <PlanetContext.Provider
+      value={ {
+        data,
+        filterByNumericValues,
+        search,
+        handleChange,
+        handleChangeFilter,
+        onClick,
+      } }
+    >
       {children}
     </PlanetContext.Provider>
   );
