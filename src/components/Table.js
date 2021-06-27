c7807469379b0b3a0dd2650ccb03d749816c2b58
bidaffonso <bidaffondo@gmail.com>
2021-06-27 13:04:39 -0400

    Req 3 Done but with complexity issues in Lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import TableContext from '../context/TableContext';
 import FiltersInsert from './FiltersInsert';
 //  Fonte ultilização de da tag <table>: https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/table
+// Fonte parteInt(): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 function Table() {
   const { planets,
     filters: {
@@ -9,6 +10,7 @@ function Table() {
     },
     filterInsert,
     filterByValue,
+    // setFilterByValue,
   } = useContext(TableContext);
 
   const tableHead = () => {
@@ -35,11 +37,64 @@ function Table() {
 
   const filterValue = filterInsert.value;
   const filterColumn = filterInsert.column;
+  const filterComparison = filterInsert.comparison;
 
   // filterPlanets();
 
   const tableBody = () => {
-    if (filterByValue === 1) {
+    if ((filterByValue === 1) && (filterComparison === 'maior que')) {
+      return (
+        planets
+        && planets
+          .filter((planet) => (nameToFilter
+            ? planet.name.includes(nameToFilter) : planet))
+          .filter((planet) => parseInt(planet[filterColumn], 10) > filterValue)
+          .map((planet, index) => (
+            <tr key={ index }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))
+      );
+    }
+    if ((filterByValue === 1) && (filterComparison === 'menor que')) {
+      return (
+        planets
+        && planets
+          .filter((planet) => (nameToFilter
+            ? planet.name.includes(nameToFilter) : planet))
+          .filter((planet) => parseInt(planet[filterColumn], 10) < filterValue)
+          .map((planet, index) => (
+            <tr key={ index }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))
+      );
+    }
+    if ((filterByValue === 1) && (filterComparison === 'igual a')) {
       return (
         planets
         && planets
