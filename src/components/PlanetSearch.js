3ab3cbdcd8def904f852eb0185ba2d49f80e97dc
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-06-27 02:43:00 -0300

    primeiro seletor

--- src/components/PlanetSearch.js
+++ src/components/PlanetSearch.js
@@ -23,6 +23,13 @@ const PlanetSearch = () => {
           onChange={ filterByName }
         />
       </label>
+      <select data-testid="column-filter">
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
     </div>
   );
 };
