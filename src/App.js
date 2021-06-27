105d729aee5ba574d14cbc70536aa2500bc24382
isTorres <ismaeltorres612@gmail.com>
2021-06-26 22:43:39 -0300

    remove all functions to componentize the app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import newPlanets from './Components/newPlanets';
-import renderTB from './Components/tableBody';
-import SWStateContext from './context/SWContext';
+import React from 'react';
+// import React, { useEffect, useState } from 'react';
+// import renderTB from './Components/tableBody';
+import TBody from './Components/TBody';
+import SWProvider from './context/SWProvider';
 import Filter from './Components/Filter';
 import './App.css';
 
 function App() {
-  const [planets, setPlanets] = useState([]);
-  const frstElemnt = planets[0];
-  // pega o 1 obj vindo da api e filtra suas keys p/ usar no cabeçalho da tabela
-  const tableHead = frstElemnt ? Object.keys(frstElemnt)
-    .filter((el) => el !== 'residents') : null;
-
-  // constroi o cabeçalho da tabela
-  const renderTH = (arrayOfData) => {
-    if (frstElemnt) {
-      return <tr>{arrayOfData.map((data, i) => <th key={ i }>{data}</th>)}</tr>;
-    }
-  };
-
-  // Faz requisição na api
-  useEffect(() => {
-    const getPlanets = async () => {
-      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
-      const { results } = await fetch(endpoint).then((data) => data.json());
-      setPlanets(results);
-    };
-    getPlanets();
-  }, [setPlanets]);
-
-  // pega o newPlanets(array da api filtrado) e mapeia p/ preencher a tabela
-  const arrayOfPlanetsData = frstElemnt
-    ? newPlanets(frstElemnt, planets, tableHead) : null;
-
   return (
-    <SWStateContext>
-      <main className="App">
-        <Filter />
-        <table className="table">
-          <thead>
-            { renderTH(tableHead) }
-          </thead>
-          <tbody>
-            { renderTB(frstElemnt, arrayOfPlanetsData) }
-          </tbody>
-        </table>
-      </main>
-    </SWStateContext>
+    <SWProvider>
+      <Filter />
+      <TBody />
+    </SWProvider>
   );
 }
 
