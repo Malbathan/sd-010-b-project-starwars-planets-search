557d5361937ce7fabba65096b52fcb35bd37acaa
Caio <caioqs.info@gmail.com>
2021-06-28 11:43:13 -0300

    Feito 3º requisito

--- src/Inputs.js
+++ src/Inputs.js
@@ -2,7 +2,12 @@ import React, { useContext } from 'react';
 import planetsContext from './context/Context';
 
 function Inputs() {
-  const { filterName } = useContext(planetsContext);
+  const {
+    filterName,
+    inputOptions,
+    optionsFilter,
+    inputOptionsFilter,
+  } = useContext(planetsContext);
   return (
     <div>
       <div>
@@ -13,6 +18,41 @@ function Inputs() {
           onChange={ filterName }
         />
       </div>
+      <div>
+        <select
+          data-testid="column-filter"
+          name="column"
+          onChange={ optionsFilter }
+        >
+          {inputOptions.map((element) => <option key={ element }>{element}</option>)}
+        </select>
+      </div>
+      <div>
+        <select
+          data-testid="comparison-filter"
+          name="comparsion"
+          onChange={ optionsFilter }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+      </div>
+      <div>
+        <input
+          data-testid="value-filter"
+          type="number"
+          name="number"
+          onChange={ optionsFilter }
+        />
+      </div>
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ inputOptionsFilter }
+      >
+        filtrar
+      </button>
     </div>
   );
 }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -3,12 +3,23 @@ import propTypes from 'prop-types';
 import planetsContext from './Context';
 
 function Provider({ children }) {
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
   const [planets, setPlanets] = useState('');
   const [planetsFilter, setPlanetsFilter] = useState('');
   const [inputPlanet, setInputPlanet] = useState({
     name: '',
+    columns: '',
+    comparison: '',
+    numberValue: '',
+
   });
-  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const [inputOptions] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
 
   async function fetchPlanets() {
     const response = await fetch(url);
@@ -25,6 +36,34 @@ function Provider({ children }) {
     setPlanetsFilter(filteredPlanets);
   };
 
+  const optionsFilter = ({ target }) => {
+    const { name, value } = target;
+    if (name === 'column') {
+      setInputPlanet({ ...inputPlanet, columns: value });
+    } else if (name === 'comparsion') {
+      setInputPlanet({ ...inputPlanet, comparison: value });
+    } else {
+      setInputPlanet({ ...inputPlanet, numberValue: value });
+    }
+  };
+
+  const inputOptionsFilter = () => {
+    const { columns, comparison, numberValue } = inputPlanet;
+    const newColuns = planetsFilter.filter((planet) => {
+      if (comparison === 'maior que') {
+        return (parseInt(planet[columns], 10) > parseInt(numberValue, 10));
+      }
+      if (comparison === 'menor que') {
+        return (parseInt(planet[columns], 10) < parseInt(numberValue, 10));
+      }
+      if (comparison === 'igual a') {
+        return (parseInt(planet[columns], 10) === parseInt(numberValue, 10));
+      }
+      return planetsFilter;
+    });
+    setPlanetsFilter(newColuns);
+  };
+
   useEffect(() => {
     fetchPlanets();
   }, []);
@@ -33,6 +72,9 @@ function Provider({ children }) {
     planets,
     planetsFilter,
     filterName,
+    inputOptions,
+    optionsFilter,
+    inputOptionsFilter,
   };
 
   return (
