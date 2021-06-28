45b8fa71428c9a8a4cda97fa551a3637465fda14
Abimaelr <abimaelr.albu@gmail.com>
2021-06-28 16:19:03 -0300

    Feature 1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+// import { array } from 'jest-runtime/node_modules/@types/yargs';
+import React, { useState, useEffect } from 'react';
+// import FetchAPI from './hooks/FetchApi';
 import './App.css';
 // Starting Project...
 function App() {
+  // const [planets, setUrl] = FetchAPI();
+  const [info, getInfo] = useState([]);
+
+  useEffect(() => {
+    const planets = async () => {
+      const { results } = await fetch('https://swapi-trybe.herokuapp.com/api/planets/').then((response) => response.json());
+      const out = results.map((result) => { delete result.residents; return result; });
+      getInfo(out);
+    };
+    planets();
+  }, []);
+  if (info.length === 0) return <div>Loading...</div>;
   return (
-    <span>Hello, App!</span>
+    <div>
+      <table>
+        <tr>{Object.keys(info[0]).map((headers, i) => <th key={ i }>{headers}</th>)}</tr>
+        {info.map((item, i) => (
+          <tr key={ i }>
+            {
+              Object.keys(item).map(
+                (key, id) => <td key={ id }>{item[key]}</td>,
+              )
+            }
+          </tr>))}
+      </table>
+    </div>
   );
 }
 
