7ae0755e56b52465f71d564761d7ed30b92b48e8
aythan azevedo <aythan16@gmail.com>
2021-06-28 20:23:04 -0300

    refatorando

--- src/services/servicesApi.js
+++ src/services/servicesApi.js
@@ -1,6 +1,5 @@
-const endPoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
-
 const reqPlanets = async () => {
+  const endPoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
   const api = await fetch(endPoint);
   const response = await api.json();
   const result = response.results;
