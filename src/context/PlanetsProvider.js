fdd14b9b7df18e11d6154a13de55d79eb0f7c68a
renanpbraga <renanpbraga@gmail.com>
2021-06-28 17:37:35 -0300

    Revert "deu ruim"
    This reverts commit 498acf6c5afa6a3e90e68b0faea55d99dcc15eac.

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
 
-    const getPlanets = () => {
-      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
-      fetch(endpoint)
-        .then((response) => response.json()
-          .then((results) => setData(results.results)));
-    };
+  const getPlanets = () => {
+    const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    fetch(endpoint)
+      .then((response) => response.json()
+        .then((results) => setData(results.results)));
+  };
   return (
-    <PlanetsContext.Provider value={{ data, setData, getPlanets }}>
+    <PlanetsContext.Provider value={ { data, setData, getPlanets } }>
       { children }
     </PlanetsContext.Provider>
-  )
+  );
 }
+
+PlanetsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default PlanetsProvider;
