a27b16647ff98aaa7d561bf4ee71e68dbb0315f3
Camila Arruda <camilaam1802@gmail.com>
2021-06-29 20:41:12 -0300

    Requisito 3 completo

--- src/components/TablePage.js
+++ src/components/TablePage.js
@@ -1,26 +1,100 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TableContext from '../context/tableContext';
 
+const comparacao = {
+  'maior que': (val1, val2) => val1 > val2,
+  'menor que': (val1, val2) => val1 < val2,
+  'igual a': (val1, val2) => val1 === val2,
+};
+
+const compareColumn = ({ comparison, column, value }, planet) => (
+  comparacao[comparison](planet[column], value)
+);
+
 function Table() {
+  const [column, setColumn] = useState('rotation_period');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState();
+
   const {
-    data, filters: { filterByName: { name } }, setFilter,
+    data, filters: {
+      filterByName: { name },
+      filterByNumericValues,
+    }, setFilter,
   } = useContext(TableContext);
   // console.log(data);
 
-  const dataFiltered = data.filter((planet) => planet.name.includes(name));
-  console.log(name);
+  const dataFiltered = data.filter((planet) => {
+    const includesName = planet.name.includes(name);
+    const includesFiltro = filterByNumericValues
+      .every((numericFilter) => compareColumn(numericFilter, planet));
+    console.log(planet.name, includesFiltro);
+    return includesName && includesFiltro;
+  });
+
+  const handleClick = () => {
+    setFilter((oldFilter) => ({ ...oldFilter,
+      filterByNumericValues: [...filterByNumericValues, {
+        column,
+        comparison,
+        value,
+      }],
+    }));
+  };
+
+  const handleChange = (event) => {
+    event.persist();
+    setFilter((oldFilter) => ({ ...oldFilter,
+      filterByName: {
+        name: event.target.value,
+      } }));
+  };
+
   if (data.length === 0) {
     return <h1>Loding...</h1>;
   }
   return (
     <main>
+      <form>
+        <select
+          data-testid="column-filter"
+          value={ column }
+          onChange={ (event) => setColumn(event.target.value) }
+        >
+          {Object.keys(data[0]).map((planet, index) => (
+            (planet === 'population'
+            || planet === 'orbital_period'
+            || planet === 'diameter'
+            || planet === 'rotation_period'
+            || planet === 'surface_water')
+              ? <option key={ index } value={ planet }>{planet}</option>
+              : 'Sem resposta'
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          value={ comparison }
+          onChange={ (event) => setComparison(event.target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          // value={ valor }
+          onChange={ (event) => setValue(event.target.value) }
+        />
+        <button type="button" data-testid="button-filter" onClick={ () => handleClick() }>
+          Adicionar Filtro
+        </button>
+      </form>
       <input
         data-testid="name-filter"
         type="text"
         value={ name }
-        onChange={ (event) => setFilter({ filterByName: {
-          name: event.target.value,
-        } }) }
+        onChange={ handleChange }
       />
       <table>
         <thead>
--- src/provider/tableProvider.js
+++ src/provider/tableProvider.js
@@ -9,8 +9,21 @@ function TableProvider({ children }) {
     filterByName: {
       name: '',
     },
+    filterByNumericValues: [
+      // {
+      //   column: '',
+      //   comparison: '',
+      //   value: 0,
+      // },
+    ],
   });
 
+  // Tenho que criar um useEffect para salvar meus estados de filtros numericos
+  useEffect(() => {
+    console.log('Foi modificado');
+  }, [filters.filterByNumericValues]);
+
+  // UseEffect para fetch daAPI e salvar os results, salvando no meu estado Data
   useEffect(() => {
     const fetchStarWarsAPI = async () => {
       const { results } = await starWarsApi();
