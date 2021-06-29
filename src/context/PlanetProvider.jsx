21366132942c346aa79db8d2352a59bfa630f957
Cristian <cristian.bugs@gmail.com>
2021-06-29 15:01:00 -0300

    API request changes

--- src/context/PlanetProvider.jsx
+++ src/context/PlanetProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-// import getApi from '../services/api';
+import getApi from '../services/api';
 import PlanetContext from './PlanetContext';
 // import response from '../testData';
 
