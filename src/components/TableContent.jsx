bcf25085eb8d0306191f52cf579580ea8157f1c8
Thiago Leite <leite.tjs@gmail.com>
2021-06-29 16:11:33 -0300

    working

--- src/components/TableContent.jsx
+++ src/components/TableContent.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function renderPlanets(filteredPlanets, colHeaders) {
-  return (
-    filteredPlanets.map((planet, key) => (
-      <tr key={ key }>
-        {
-          colHeaders.map((head) => (
-            <td key={ head }>{planet[head]}</td>
-          ))
-        }
-      </tr>
-    )));
+  return filteredPlanets.map((planet, key) => (
+    <tr key={ key }>
+      {
+        colHeaders.map((head) => (
+          <td key={ head }>{planet[head]}</td>
+        ))
+      }
+    </tr>
+  ));
 }
 
 const TableContent = () => (
