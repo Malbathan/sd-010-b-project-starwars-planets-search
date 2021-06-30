7c6ceac56a9205315dad9d5987e60c2792e92be2
Abimaelr <abimaelr.albu@gmail.com>
2021-06-29 21:36:04 -0300

    Feature 2 Done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-// import { array } from 'jest-runtime/node_modules/@types/yargs';
-import React, { useState, useEffect } from 'react';
-// import FetchAPI from './hooks/FetchApi';
+import React from 'react';
+import Table from './components/Table';
 import './App.css';
-// Starting Project...
-function App() {
-  // const [planets, setUrl] = FetchAPI();
-  const [info, getInfo] = useState([]);
+import PlanetProvider from './context/PlanetProvider';
+import Forms from './components/Forms';
 
-  useEffect(() => {
-    const planets = async () => {
-      const { results } = await fetch('https://swapi-trybe.herokuapp.com/api/planets/').then((response) => response.json());
-      const out = results.map((result) => { delete result.residents; return result; });
-      getInfo(out);
-    };
-    planets();
-  }, []);
-  if (info.length === 0) return <div>Loading...</div>;
+function App() {
   return (
-    <div>
-      <table>
-        <tr>{Object.keys(info[0]).map((headers, i) => <th key={ i }>{headers}</th>)}</tr>
-        {info.map((item, i) => (
-          <tr key={ i }>
-            {
-              Object.keys(item).map(
-                (key, id) => <td key={ id }>{item[key]}</td>,
-              )
-            }
-          </tr>))}
-      </table>
-    </div>
+    <PlanetProvider>
+      <Forms />
+      <Table />
+    </PlanetProvider>
   );
 }
 
