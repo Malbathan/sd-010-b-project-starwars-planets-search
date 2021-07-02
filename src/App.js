c2425b9f3c9693fcfd6b4504182dd712d32f5905
CamilaCode7 <CamilaCode7>
2021-07-01 22:12:33 -0300

    Adicionando Provider e os componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Provider from './context/myProvider';
+import Table from './components/table';
+import Filter from './components/filtros';
 import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Provider>
+      <main>
+        <span>Star Wars Planets</span>
+        <Filter />
+        <Table />
+      </main>
+    </Provider>
   );
 }
 
