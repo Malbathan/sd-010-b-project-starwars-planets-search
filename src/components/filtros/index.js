720ed571207fda41197c26e8f90f3fb4d1166b78
CamilaCode7 <CamilaCode7>
2021-07-02 17:23:50 -0300

    adicionando requisito 2

--- src/components/filtros/index.js
+++ src/components/filtros/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import TextFilter from './textFIlter';
+import Options from './options';
+import Filters from './filters';
 
 function Filter() {
   return (
     <div>
       <TextFilter />
+      <Options />
+      <Filters />
     </div>
   );
 }
